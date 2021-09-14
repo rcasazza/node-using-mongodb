1cffb35b03a9b80ed0528ccb3305f9c0f55ef560
Bob Casazza <rcasazza@gmail.com>
2021-09-14 13:04:53 -0400

    add mongodb and mongoose connection

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = 4000;
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/productsdb',
+    {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    }
+);
 
 app.get('/', (req, res) =>
     res.send(`Store server running on port ${PORT}`)
 );
 
-app.listen(PORT, () => 
+app.listen(PORT, () =>
     console.log(`Your server is running on port ${PORT}`)
 );
